7c4ca5b4a4c37fff4736e2ca194e2393dcaba515
kuhuhalder <kuhuhalder2701@gmail.com>
2021-03-15 15:39:49 -0400

    changes

--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -6,14 +6,53 @@
 //
 
 import UIKit
-
-class FeedViewController: UIViewController {
-
+import AlamofireImage
+import Parse
+class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
+    var posts = [PFObject]()
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.delegate = self
+        tableView.dataSource = self
         // Do any additional setup after loading the view.
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let query = PFQuery(className: "Posts")
+        query.includeKey("author")
+        query.limit = 20
+        query.findObjectsInBackground
+        {(posts, error) in
+            if posts != nil{
+                self.posts = posts!
+                self.tableView.reloadData()
+            }
+        
+        
+        
+    }
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+        
+        
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let post  = posts[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+        
+        let user = post["author"] as! PFUser
+        cell.usernameLabel.text = user.username
+        cell.captionLabel.text = post["caption"] as! String
+        let imageFile = post["image"] as! PFFileObject
+        let urlString = imageFile.url!
+        let url = URL(string: urlString)!
+        cell.photoView.af_setImage(withURL: url)
+        return cell
+    }
+}
     
 
     /*
@@ -26,4 +65,5 @@ class FeedViewController: UIViewController {
     }
     */
 
-}
+
+
--- Parstagram/LoginViewController.swift
+++ Parstagram/LoginViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 class LoginViewController: UIViewController {
 
@@ -14,14 +15,35 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var passwordField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
     @IBAction func onSignIn(_ sender: Any) {
+        let username = usernameField.text!
+        let password = passwordField.text!
+        PFUser.logInWithUsername(inBackground: username, password: password){(user, error) in
+            if user != nil{
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            }
+            else{
+                print("Error: \(error?.localizedDescription)")
+            }
+        }
     }
     
+    
     @IBAction func onSignUp(_ sender: Any) {
+        let user = PFUser()
+        user.username = usernameField.text
+        user.password = passwordField.text
+        user.signUpInBackground{(success, error) in
+            if success{
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            } else{
+                print("Error: \(error?.localizedDescription)")
+            }
+    }
     }
     /*
     // MARK: - Navigation
@@ -34,3 +56,4 @@ class LoginViewController: UIViewController {
     */
 
 }
+
